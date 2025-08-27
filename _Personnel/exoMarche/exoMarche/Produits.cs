416f2a2a038e30c04b8dd65e5347200245b9a37c
Mateo MID2B <pn41htu@eduvaud.ch>
2025-08-27 12:06:17 +0200

    ex1 marché - p. fonctionelle

---
+++ _Personnel/exoMarche/exoMarche/Produits.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exoMarche
+{
+    internal class Produit
+    {
+            public int Emplacement { get; set; }
+            public string Producteur { get; set; }
+            public string NomProduit { get; set; }
+            public int Quantite { get; set; }
+            public string Unité { get; set; }
+            public double PrixParUnite { get; set; }
+
+    }
+}
