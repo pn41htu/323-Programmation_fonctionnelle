0b210be8b577c6bfbbfca731c77c58fe052fdae5
Mateo <pn41htu@eduvaud.ch>
2025-09-03 10:32:48 +0200

    import exo cinéma dans repo perso

---
+++ _Personnel/exo_cinema/exo_cinema/exo_cinema/Movie.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exo_cinema
+{
+    internal class Movie
+    {
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        public double Rating { get; set; }
+        public int Year { get; set; }
+        public string[] LanguageOptions { get; set; }
+        public string[] StreamingPlatforms { get; set; }
+
+    }
+}
