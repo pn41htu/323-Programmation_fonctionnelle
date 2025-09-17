548c765e0ef661c57ecf61f5bc90606d73398431
Mateo <pn41htu@eduvaud.ch>
2025-09-17 10:49:59 +0200

    exo : marketIs back [WIP]
    Le PC a crash et j'ai perdu ce que j'avais fait

---
+++ _Personnel/marketIsBack/marketIsBack/Products.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace marketIsBack
+{
+    internal class Products
+    {
+        public int Location { get; set; }
+        public string Producer { get; set; }
+        public string ProductsName { get; set; }
+        public int Quantity { get; set; }
+        public string Unit { get; set; }
+        public double PricePerUnit { get; set; }
+
+    }
+}
---
+++ _Personnel/marketIsBack/marketIsBack/Program.cs
@@ -0,0 +1,18 @@
+﻿using marketIsBack;
+
+
+List<Products> Products = new List<Products>
+{
+    new Products { Location = 1, Producer = "Bornand", ProductsName = "Pommes", Quantity = 20,Unit = "kg", PricePerUnit = 5.50 },
+    new Products { Location = 1, Producer = "Bornand", ProductsName = "Poires", Quantity = 16,Unit = "kg", PricePerUnit = 5.50 },
+    new Products { Location = 1, Producer = "Bornand", ProductsName = "Pastèques", Quantity = 14,Unit = "pièce", PricePerUnit = 5.50 },
+    new Products { Location = 1, Producer = "Bornand", ProductsName = "Melons", Quantity = 5,Unit = "kg", PricePerUnit = 5.50 },
+    new Products { Location = 2, Producer = "Dumont", ProductsName = "Noix", Quantity = 20,Unit = "sac", PricePerUnit = 5.50 },
+    new Products { Location = 2, Producer = "Dumont", ProductsName = "Raisin", Quantity = 6,Unit = "kg", PricePerUnit = 5.50 },
+    new Products { Location = 2, Producer = "Dumont", ProductsName = "Pruneaux", Quantity = 13,Unit = "kg", PricePerUnit = 5.50 },
+    new Products { Location = 2, Producer = "Dumont", ProductsName = "Myrtilles", Quantity = 12,Unit = "kg", PricePerUnit = 5.50 },
+
+    //...
+};
+
+
